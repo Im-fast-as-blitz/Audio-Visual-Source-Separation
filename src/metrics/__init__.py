ac1f78535287decba3e006fe36fb264379c0b629
alinaaugust <alinaavgustenok@gmail.com>
2024-11-09 20:15:37 +0300

    add metrics

--- src/metrics/__init__.py
+++ src/metrics/__init__.py
@@ -1,1 +1,12 @@
 from src.metrics.example import ExampleMetric
+from src.metrics.sisnri import SI_SNRI
+from src.metrics.sdri import SDR
+from src.metrics.pesq import PESQ
+from src.metrics.stoi import STOI
+
+__all__ = [
+    "SI_SNRI",
+    "SDR",
+    "PESQ",
+    "STOI"
+]
---
+++ src/metrics/sdri.py
@@ -0,0 +1,28 @@
+import torch
+from torchmetrics import SignalDistortionRatio
+from src.metrics.base_metric import BaseMetric
+
+class SDR(BaseMetric):
+    def __init__(self, device:str, *args, **kwargs):
+        """
+        Computes SDR metric
+
+        Args:
+            device (str): device for the metric calculation (and tensors).
+        """
+        super().__init__(*args, **kwargs)
+        if device == "auto":
+            device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.metric = SignalDistortionRatio().to(device)
+
+    def __call__(self, preds: torch.Tensor, target: torch.Tensor, **kwargs):
+        """
+        Takes predicted audio and target audio and returns SDR value.
+
+        Args:
+            preds (Tensor): predicted audio.
+            target (Tensor): target audio.
+        Returns:
+            metric (float): calculated metric.
+        """
+        return self.metric(preds, target).mean().item()
