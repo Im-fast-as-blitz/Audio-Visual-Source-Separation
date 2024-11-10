14ea0e32744c62d78183a78917179b4df9778a24
alinaaugust <alinaavgustenok@gmail.com>
2024-11-10 17:18:51 +0300

    fix some metrics & add metrics config

--- src/metrics/__init__.py
+++ src/metrics/__init__.py
@@ -1,12 +1,12 @@
 from src.metrics.example import ExampleMetric
-from src.metrics.sisnri import SI_SNRI
-from src.metrics.sdri import SDR
+from src.metrics.sisnri import SI_SNRi
+from src.metrics.sdri import SDRi
 from src.metrics.pesq import PESQ
 from src.metrics.stoi import STOI
 
 __all__ = [
-    "SI_SNRI",
-    "SDR",
+    "SI_SNRi",
+    "SDRi",
     "PESQ",
     "STOI"
 ]
--- src/metrics/sisnri.py
+++ src/metrics/sisnri.py
@@ -2,7 +2,7 @@ import torch
 from torchmetrics import ScaleInvariantSignalNoiseRatio
 from src.metrics.base_metric import BaseMetric
 
-class SI_SNRI(BaseMetric):
+class SI_SNR(BaseMetric):
     def __init__(self, device:str, *args, **kwargs):
         """
         Computes SI-SNRi metric
@@ -26,3 +26,30 @@ class SI_SNRI(BaseMetric):
             metric (float): calculated metric.
         """
         return self.metric(preds, target).mean().item()
+    
+
+class SI_SNRi(BaseMetric):
+    def __init__(self, device:str, *args, **kwargs):
+        """
+        Computes SI-SNRi metric
+
+        Args:
+            device (str): device for the metric calculation (and tensors).
+        """
+        super().__init__(*args, **kwargs)
+        if device == "auto":
+            device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.metric = SI_SNR(device)
+
+    def __call__(self, input: torch.Tensor, preds: torch.Tensor, target: torch.Tensor, **kwargs):
+        """
+        Takes input, predicted and target audio and returns SI-SNRi value.
+
+        Args:
+            input (Tensor): input audio.
+            preds (Tensor): predicted audio.
+            target (Tensor): target audio.
+        Returns:
+            metric (float): calculated metric.
+        """
+        return self.metric(preds, target) - self.metric(input, target)
