305a94e7db4608a4ae9b563965967da984eda58e
Im-fast-as-blitz <rodion.chernomordin@icloud.com>
2024-11-21 21:45:59 +0300

    chaneg configs

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,7 +1,9 @@
 from src.model.baseline_model import BaselineModel
 from src.model.conv_tasnet import ConvTasNetModel
+from src.model.rtfs-net import RTFSNetModel
 
 __all__ = [
     "BaselineModel",
-    "ConvTasNetModel"
+    "ConvTasNetModel",
+    "RTFSNetModel"
 ]
